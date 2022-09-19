b5b3a2fd1796f4ac910e9bac4d2bb82901bdb16f
Luca Santana <luca.santana@adaptersistemas.com.br>
2022-09-19 17:59:30 -0300

    Create SellerService e index view of Seller

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService sellerService;
+
+        public SellersController(SellerService service)
+        {
+            sellerService = service;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = sellerService.FindAll();
+            return View(list);
         }
     }
 }
--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWeb.Models;
 using SalesWeb.Data;
+using SalesWeb.Services;
 
 namespace SalesWeb
 {
@@ -42,6 +41,7 @@ namespace SalesWeb
                 builder => builder.MigrationsAssembly("SalesWeb")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
