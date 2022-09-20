81b2a421f2cefee79be4024f2bd4d20ffe3f0424
Luca Santana <luca.santana@adaptersistemas.com.br>
2022-09-20 15:49:50 -0300

    Simple and grouping search

--- SalesWeb/Controllers/SaleRecordsController.cs
+++ SalesWeb/Controllers/SaleRecordsController.cs
@@ -3,24 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
 {
     public class SaleRecordsController : Controller
     {
+        private readonly SaleRecordsService _saleRecordService;
+
+        public SaleRecordsController(SaleRecordsService saleRecordsService)
+        {
+            _saleRecordService = saleRecordsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _saleRecordService.FindByDateAsync(minDate, maxDate);
+
+            return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _saleRecordService.FindByDateGroupingAsync(minDate, maxDate);
+
+            return View(result);
         }
     }
 }
---
+++ SalesWeb/Services/SaleRecordsService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
+
+namespace SalesWeb.Services
+{
+    public class SaleRecordsService
+    {
+        private readonly SalesWebContext _context;
+
+        public SaleRecordsService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SaleRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from saleRecord in _context.SaleRecord select saleRecord;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SaleRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from saleRecord in _context.SaleRecord select saleRecord;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+    }
+}
